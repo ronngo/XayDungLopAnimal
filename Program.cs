168390d00455b76f9190505f616db78c374bb26c
ronngo <ronngomn23@gmail.com>
2020-09-21 18:46:08 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace XayDungLopAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Animal animal2 = new Animal("Tiger", "Meo meo");
+
+            animal.GetInfo();
+            
+            animal2.GetInfo();
+        }
+    }
+}
